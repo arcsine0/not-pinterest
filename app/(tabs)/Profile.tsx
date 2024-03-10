9553a6deebbedc0f5894e93eba167254014db1c2
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-11 00:27:50 +0800

    Profile now reflects logged data

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -10,17 +10,32 @@ import CollectionCard from "@/components/CollectionCard";
 import { doc, collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/config";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 type Thumbnail = {
 	name: string;
 	url: string;
 }
 
 export default function Profile() {
+	const [displayName, setDisplayName] = useState("DisplayName");
+	const [userName, setUserName] = useState("username");
+
 	const [collectionThumbs, setCollectionThumbs] = useState<Thumbnail[]>([]);
 
 	const defaultThumb = "https://images.unsplash.com/photo-1709833226150-8eaeb6f291d1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 	useEffect(() => {
+		const getData = async () => {
+			const dataStr = await AsyncStorage.getItem("data");
+			if (dataStr) {
+				const data = JSON.parse(dataStr);
+				setDisplayName(data.displayName);
+				setUserName(data.userName);
+			}
+		}
+		getData();
+
 		getDocs(collection(db, "Liked"))
 			.then((sn) => {
 				const thumb: Thumbnail = {
@@ -42,8 +57,8 @@ export default function Profile() {
 							uri="https://source.boringavatars.com/beam/"
 							className="w-1/3"
 						/>
-						<Text className="text-3xl mt-5 dark:text-white font-bold">ProfileName</Text>
-						<Text className="text-lg dark:text-white">@username</Text>
+						<Text className="text-3xl mt-5 dark:text-white font-bold">{displayName}</Text>
+						<Text className="text-lg dark:text-white">@{userName}</Text>
 					</View>
 					<View className="flex flex-col w-full mt-20 gap-2">
 						<Text className="text-3xl dark:text-white font-bold">Collections</Text>
